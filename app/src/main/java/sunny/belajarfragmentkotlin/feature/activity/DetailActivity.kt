2140a425e47e4506ecce093aeed0301edba8a2fd
battkax93 <sunny,putra93@gmail.com>
2018-11-06 17:14:07 +0700

    fix retrofit kotlin

---
+++ app/src/main/java/sunny/belajarfragmentkotlin/feature/activity/DetailActivity.kt
@@ -0,0 +1,13 @@
+package sunny.belajarfragmentkotlin.feature.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import sunny.belajarfragmentkotlin.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
