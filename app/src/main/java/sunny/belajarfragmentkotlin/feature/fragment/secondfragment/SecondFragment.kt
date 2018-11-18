b53a3d3b27dcd544fc1ef842b4294702b2bbb2d8
choputra <choputra@github.com>
2018-11-18 10:17:21 +0700

    add Gesture Listener

--- app/src/main/java/sunny/belajarfragmentkotlin/feature/fragment/secondfragment/SecondFragment.kt
+++ app/src/main/java/sunny/belajarfragmentkotlin/feature/fragment/secondfragment/SecondFragment.kt
@@ -1,19 +1,24 @@
 package sunny.belajarfragmentkotlin.feature.fragment.secondfragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
+import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.coroutines.*
+import android.widget.Button
 import sunny.belajarfragmentkotlin.R
+import sunny.belajarfragmentkotlin.feature.fragment.GestureListener
 import java.util.*
 import kotlin.concurrent.schedule
-import kotlin.concurrent.thread
-import kotlin.coroutines.coroutineContext
 
 class SecondFragment : Fragment() {
+
+    lateinit var gestureView:  Button
+    lateinit var second: View
+    lateinit var fragFirst: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +27,23 @@ class SecondFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val second = inflater.inflate(R.layout.fragment_second, container, false)
+        second = inflater.inflate(R.layout.fragment_second, container, false)
         tx()
+        Gesture(second)
         return second
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun Gesture(v: View){
+        gestureView = v.findViewById(R.id.b_2)
+
+        val gestureListener = GestureListener(gestureView)
+        val gd = GestureDetector(activity, gestureListener)
+
+        gestureView.setOnTouchListener { view, motionEvent ->
+            gd.onTouchEvent(motionEvent)
+            false
+        }
     }
 
     fun tx() {
